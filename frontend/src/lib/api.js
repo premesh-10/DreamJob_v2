36dde3b4cf4f8ceece9e7aa1dc6f7e635762f9b4
premesh-10 <166977910+premesh-10@users.noreply.github.com>
2026-06-01 23:36:30 +0530

    Updated url

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api/v1';
+const API_BASE_URL = 'https://dream-job-v2.vercel.app';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
