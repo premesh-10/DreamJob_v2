5d294d2c5f86ba94fd215e30b7aab3caa3ed79e0
premesh-10 <166977910+premesh-10@users.noreply.github.com>
2026-06-01 23:51:49 +0530

    Updated url

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://dreamjob-v2-hrdh.onrender.com';
+const API_BASE_URL = 'https://dreamjob-v2-hrdh.onrender.com/api/v1';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
